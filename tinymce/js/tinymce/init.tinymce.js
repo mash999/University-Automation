7acfdd0eb77054b81cfa592e35bd0a3b91cdb59e
Ruhul Mashbu <mashbu111@gmail.com>
2020-02-03 01:15:32 -0330

    Add files via upload

---
+++ tinymce/js/tinymce/init.tinymce.js
@@ -0,0 +1,8 @@
+
+tinymce.init({ 
+	selector:'textarea.tinymce',
+	width : "100%",
+	height : 300,
+	plugins : ["link","lists","hr","anchor","fullscreen","textcolor","insertdatetime","contextmenu","wordcount","table"]
+
+});
